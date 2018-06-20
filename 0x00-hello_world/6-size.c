cb9846b59067125af851c45ef86894a798df4db9
thenicopixie <391@holbertonschool.com>
2018-06-20 21:14:31 +0000

    print byte size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,8 @@ int main(void)
 	printf("Size of a char: %li byte(s)\n", sizeof(char));
 	printf("Size of an int: %li byte(s)\n", sizeof(int));
 	printf("Sizie of a long int: %li byte(s)\n", sizeof(long));
-	printf("Size of a long long int: %lli byte(s)\n", sizeof(long long));
-	printf("Size of a float: %f byte(s)\n", sizeof(float));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(long long));
+	printf("Size of a float: %li byte(s)\n", sizeof(float));
+	
 	return (0);
 }
