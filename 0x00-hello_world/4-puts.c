829fec10b1a9e22de284e6ce3b3b4044a0520f00
thenicopixie <391@holbertonschool.com>
2018-06-20 19:55:17 +0000

    program that prints text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #!/bin/bash
-int main()
+int main(void)
 {
-	int puts ("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
