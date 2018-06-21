4bbdf3aa0a132f33d7ca1381e394ebb0ade45611
thenicopixie <391@holbertonschool.com>
2018-06-21 22:14:49 +0000

    print icomplete alphabet with ASCII

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* program to print incomplete alphabet
+* Return: 0
+*/
+int main(void)
+{
+	int letter;
+
+	for (letter = 97; letter <= 122; letter++)
+	{
+		if (letter == 101 || letter == 114)
+		{
+			continue;
+		}
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
