43ce3e4752a222bef6a7d16529a5d2b3dfd49861
thenicopixie <391@holbertonschool.com>
2018-06-21 20:27:56 +0000

    find last digit in num

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* print last digit in int
+* Answer: 0
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	int i = n % 10;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (i > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, i);
+	}
+	else if (i == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, i);
+	}
+	else if (i < 6 && i != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
+	}
+	return (0);
+}
