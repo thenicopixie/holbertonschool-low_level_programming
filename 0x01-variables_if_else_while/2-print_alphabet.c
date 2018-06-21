6184b952236ef7bff8d3ddd6dfac80a26aa74c47
thenicopixie <391@holbertonschool.com>
2018-06-21 21:21:56 +0000

    print a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* program: print a-z
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
