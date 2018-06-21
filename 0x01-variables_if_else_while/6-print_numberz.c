4c4c97e73d3540cfee89e57a5810bbba29f84f52
thenicopixie <391@holbertonschool.com>
2018-06-21 23:18:06 +0000

    print 0-9 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* print 0-9
+* Result: 0;
+*/
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
