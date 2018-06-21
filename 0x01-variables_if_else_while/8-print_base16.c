fed4bd624ceb91e184c4bf94b45754f94dacd969
thenicopixie <391@holbertonschool.com>
2018-06-21 23:45:08 +0000

    print 0-9a-z

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* print numbers in lowercase
+* Return: 0
+*/
+
+int main(void)
+{
+	int nums = 0;
+	int letter = 97;
+
+	while (nums <= 16)
+	{
+		putchar(nums);
+	}
+	while (letter <= 122)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
