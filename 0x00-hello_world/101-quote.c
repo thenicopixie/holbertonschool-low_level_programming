cfc83195799f68920ad32f738053ed9cc5740c88
thenicopixie <391@holbertonschool.com>
2018-06-21 07:44:57 +0000

    program the prints to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*
-*
-*
+* main - Entry point
+* print text
+* Return:  1 (Success)
 */
 int main(void)
 {
-	putchar("and that piece of art is useful %c - Dora Korpar, 2015-10-19", '"');
-	return(0);
+	fprintf(stderr, "and that piece of art is useful %c - Dora Korpar, 2015-10-19", '"');
+	return (0);
 }
