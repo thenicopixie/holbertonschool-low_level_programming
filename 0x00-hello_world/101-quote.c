6aeaedf1a8aa4438bd08d51dc2d75ba833f23b3d
thenicopixie <391@holbertonschool.com>
2018-06-22 20:01:26 +0000

    print text with write command

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 /**
 * main - Entry point
@@ -6,6 +7,6 @@
 */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful %c - Dora Korpar, 2015-10-19", '"');
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	return (1);
 }
