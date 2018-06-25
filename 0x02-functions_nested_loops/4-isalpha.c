54521a29c663d67b70e56611ee106ff129e64221
thenicopixie <391@holbertonschool.com>
2018-06-25 23:47:38 +0000

    function to check if character is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <ctype.h>
 /**
- * _isalpha - entry point
+* _isalpha - entry point
  * program to check for alpha letters
  * @c: the character to test
  * Return: 1 if alpha, 0 if otherwise
