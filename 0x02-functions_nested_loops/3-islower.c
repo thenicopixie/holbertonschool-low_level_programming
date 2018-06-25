4849721bb6f287abaf662a0ff1f3666c69717def
thenicopixie <391@holbertonschool.com>
2018-06-25 23:27:57 +0000

    function to check is character is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <ctype.h>
+/**
+* _islower - check for lowercase character
+* @c: character to test
+* Return: 0
+*/
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
