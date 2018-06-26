474f0f497c040cf1da4b2cb23b82bcd236be5f3e
thenicopixie <391@holbertonschool.com>
2018-06-26 07:23:37 +0000

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* add -  add two integers
+* @num1: first integer
+* @num2: second integer
+*/
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+
+	return (result);
+}
