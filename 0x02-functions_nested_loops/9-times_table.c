4514b05d390f736096b7b299d3ddfac93de13514
thenicopixie <391@holbertonschool.com>
2018-06-26 21:15:31 +0000

    print 9x table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,44 +1,49 @@
 #include "holberton.h"
 /**
-* times_table - print 9x times table
+* times_table - print 9x table
+*
 */
 void times_table(void)
 {
 	int num1, num2, mult_num, dig1, dig2;
 
-	num1 = 0;
-
-	do {
-	num2 = 0;
-
-		do {
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
 			mult_num = num1 * num2;
-			if (mult_num <= 9)
-			{
-				_putchar(' ');
-				_putchar(mult_num + '0');
-			}
-			else
-			{
 			dig1 = mult_num / 10;
 			dig2 = mult_num % 10;
-			_putchar(dig1 + '0');
-			_putchar(dig2 + '0');
+			if (mult_num == 0)
+			{
+				if (num1 == 0 && num2 > 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(dig2 + '0');
+				if (num2 < 9)
+				{
+				_putchar(',');
+				_putchar(' ');
+				}
 			}
-			if (num2 < 9)
+			else if (mult_num <= 9)
+			{
+				_putchar(' ');
+				_putchar(dig2 + '0');
+			}
+			else if (mult_num > 9)
+			{
+				_putchar(dig1 + '0');
+				_putchar(dig2 + '0');
+			}
+			if (num2 < 9 && mult_num > 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			num2 = num2 + 1;
-		} while (num2 <= 9);
-		{
-			_putchar('\n');
-			num1 = num1 + 1;
 		}
-	} while (num1 <= 9);
-		{
-			_putchar(dig1 + '0');
-			_putchar(dig2 + '0');
-		}
+		_putchar('\n');
+	}
+
 }
