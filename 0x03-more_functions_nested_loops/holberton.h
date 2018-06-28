83dcfe15503ccea16926bbad3dd63263b53aeaa9
thenicopixie <391@holbertonschool.com>
2018-06-28 17:38:59 +0000

    update header file

--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -4,9 +4,16 @@
 /**
 * _putchar - print character
 * 0. 0-isupper.c - function to check for uppercase letter
-*
+* 1. int _isdigit(int c) - check if character is digit 
+* 2. int mul(int a, int b) - multiply two integers
+* 3. void print_numbers(void) - print numbers 0-9
+* 4.
+* 5.
 */
 int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
 
 #endif /* _HOLBERTON_H_ */
