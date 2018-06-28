9024d6737fe589cb26d51cea03603aa3e4045b4b
thenicopixie <391@holbertonschool.com>
2018-06-28 18:05:09 +0000

    update header file

--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -3,12 +3,17 @@
 
 /**
 * _putchar - print character
-* 0. 0-isupper.c - function to check for uppercase letter
-* 1. int _isdigit(int c) - check if character is digit 
-* 2. int mul(int a, int b) - multiply two integers
-* 3. void print_numbers(void) - print numbers 0-9
-* 4. void print_most_numbers(void) - print numbers 0-1, 3, 5-9
-* 5. void more_numbers(void) - print 0-14 ten times
+ *  0. 0-isupper.c - function to check for uppercase letter
+ *  1. int _isdigit(int c) - check if character is digit 
+ *  2. int mul(int a, int b) - multiply two integers
+ *  3. void print_numbers(void) - print numbers 0-9
+ *  4. void print_most_numbers(void) - print numbers 0-1, 3, 5-9
+ *  5. void more_numbers(void) - print 0-14 ten times
+ *  6. void print_line(int n) - draw straight line
+ *  7. void print_diagonal(int n) - draw diagonal line
+ *  8. void print_square(int size) - print square
+ *  9.
+ * 10. void print_triangle(int size) - print triangle
 */
 int _putchar(char c);
 int _isupper(int c);
@@ -17,5 +22,9 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
 #endif /* _HOLBERTON_H_ */
