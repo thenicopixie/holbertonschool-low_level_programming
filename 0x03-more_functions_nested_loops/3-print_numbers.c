e4118eec0e88592bf87043655fe38dd576c80eeb
thenicopixie <391@holbertonschool.com>
2018-06-28 17:38:34 +0000

    print numbers 0-9

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* print_numbers - print numbers 0-9
+*/
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
