1c36a992b58196eb5f0a2d751ccc342f688c1182
thenicopixie <391@holbertonschool.com>
2018-06-28 17:40:38 +0000

    update header file

--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -7,7 +7,7 @@
 * 1. int _isdigit(int c) - check if character is digit 
 * 2. int mul(int a, int b) - multiply two integers
 * 3. void print_numbers(void) - print numbers 0-9
-* 4.
+* 4. void print_most_numbers(void) - print numbers 0-1, 3, 5-9
 * 5.
 */
 int _putchar(char c);
@@ -15,5 +15,6 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 
 #endif /* _HOLBERTON_H_ */
