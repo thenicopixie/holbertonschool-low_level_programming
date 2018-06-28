1060004fe08e1197e06a3644847b592be6bc2ce6
thenicopixie <391@holbertonschool.com>
2018-06-28 17:17:28 +0000

    add header file

---
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+* _putchar - print character
+* 0. 0-isupper.c - function to check for uppercase letter
+*
+*/
+int _putchar(char c);
+int _isupper(int c);
+
+#endif /* _HOLBERTON_H_ */
