61b9d81b579d202311b6d9e1d26d98718a67a40c
thenicopixie <391@holbertonschool.com>
2018-07-03 04:32:43 +0000

    print half of string

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * puts_half - print half of string
+ * @str: string to print
+ */
+void puts_half(char *str)
+{
+	int i, len;
+
+	len = _strlen(str);
+
+	if (len % 2 == 0)
+	{
+		i = len / 2;
+	}
+	else if (len % 2 != 0)
+	{
+		i = (len - 1) / 2;
+	}
+	for (; i <= len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+/**
+ * _strlen - return length of string
+ * @s: string to check
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	;
+	return (length);
+}
