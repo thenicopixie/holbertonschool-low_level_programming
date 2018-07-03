6ce14b0ea8a9329fe0012a7c4dc4c11239908511
thenicopixie <391@holbertonschool.com>
2018-07-03 07:09:53 +0000

    find length of string

--- 0x04-pointers_arrays_strings/2-strlen.c
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 /**
-* _strlen - return length of string
-* @s: string
-*/
+ * _strlen - return length of string
+ * @s: string
+ * Return: length
+ */
 int _strlen(char *s)
 {
 	int length;
 
-	for (length = 0; s[length] != '\0'; length++);
+	for (length = 0; s[length] != '\0'; length++)
+	;
 
 	return (length);
 }
