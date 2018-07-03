866b7a8eb2f28b2e0e45f6c9b2b25f430f4a17d6
thenicopixie <391@holbertonschool.com>
2018-07-03 07:04:40 +0000

    update header file

---
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -0,0 +1,29 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * 0. void reset_to_98(int *n) - update value with pointer
+ * 1. void swap_int(int *a, int *b) - swap integers
+ * 2. int _strlen(char *s) - return length of string
+ * 3. void _puts(char *str) - print string
+ * 4. void print_rev(char *s) - print string in reverse
+ * 5. void rev_string(char *s) - reverse string
+ * 6. void puts2(char *str) - print one char out of 2 of a string
+ * 7. void puts_half(char *str) - print half of string
+ * 8. void print_array(int *a, int n) - print n elements of an array
+ * 9. char *_strcpy(char *dest, char *src) - copy string to buffer
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif /*_HOLBERTON_H */
