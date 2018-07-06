65173168a46e95cb7a86b2db80e36d7ddbc6d938
thenicopixie <391@holbertonschool.com>
2018-07-06 18:55:07 +0000

    compare two strings

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -14,15 +14,16 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (s1[i] == s2[i])
-		{
-			return (0);
-		}
 		if (s1[i] != s2[i])
 		{
 			diff =  s1[i] - s2[i];
 			break;
 		}
+		else
+		{
+		diff = 0;
+		}
+		i++;
 	}
 	return (diff);
 }
