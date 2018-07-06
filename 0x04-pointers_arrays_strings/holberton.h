9284f35831516436fc28fe66e23a2ab78e93a19c
thenicopixie <391@holbertonschool.com>
2018-07-06 05:07:04 +0000

    update header file

--- 0x04-pointers_arrays_strings/holberton.h
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -2,16 +2,18 @@
 #define _HOLBERTON_H_
 
 /**
- * 0. void reset_to_98(int *n) - update value with pointer
- * 1. void swap_int(int *a, int *b) - swap integers
- * 2. int _strlen(char *s) - return length of string
- * 3. void _puts(char *str) - print string
- * 4. void print_rev(char *s) - print string in reverse
- * 5. void rev_string(char *s) - reverse string
- * 6. void puts2(char *str) - print one char out of 2 of a string
- * 7. void puts_half(char *str) - print half of string
- * 8. void print_array(int *a, int n) - print n elements of an array
- * 9. char *_strcpy(char *dest, char *src) - copy string to buffer
+ *  0. void reset_to_98(int *n) - update value with pointer
+ *  1. void swap_int(int *a, int *b) - swap integers
+ *  2. int _strlen(char *s) - return length of string
+ *  3. void _puts(char *str) - print string
+ *  4. void print_rev(char *s) - print string in reverse
+ *  5. void rev_string(char *s) - reverse string
+ *  6. void puts2(char *str) - print one char out of 2 of a string
+ *  7. void puts_half(char *str) - print half of string
+ *  8. void print_array(int *a, int n) - print n elements of an array
+ *  9. char *_strcpy(char *dest, char *src) - copy string to buffer
+ * 10. int _atoi(char *s)
+ * 11.
  */
 
 int _putchar(char c);
@@ -25,5 +27,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif /*_HOLBERTON_H */
