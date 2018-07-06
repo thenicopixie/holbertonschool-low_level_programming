56ef23da2e9e74a314199095677400f85cde4425
thenicopixie <391@holbertonschool.com>
2018-07-06 21:47:52 +0000

    update header file

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -9,7 +9,7 @@
  * 4. void reverse_array(int *a, int n) - reverse content of an array of integers
  * 5. char *string_toupper(char *) - change lowercase letters to uppercase
  * 6. char *cap_string(char *) - capitalize all words of a string
- * 7. 
+ * 7. char *leet(char *) - encode string to 1337 
  * 8. char *rot13(char *) - encode string using rot13
  */
 
@@ -21,7 +21,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-
+char *leet(char *)
 char *rot13(char *);
 
 #endif /* _HOLBERTON_H_ */
