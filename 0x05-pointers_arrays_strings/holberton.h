bf5f02004c2e34743817c844008ec0d14edd5286
thenicopixie <391@holbertonschool.com>
2018-07-06 15:49:37 +0000

    update header file

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -13,6 +13,7 @@
  * 8. char *rot13(char *) - encode string using rot13
  */
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
