e9ee00253123584b9a1531141f3d28bd3c660bd8
thenicopixie <391@holbertonschool.com>
2018-07-07 19:54:09 +0000

    keep tabs

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -20,11 +20,12 @@ char *cap_string(char *p)
 			if (p[i] == seper[j] && p[i + 1] >= 'a' && p[i + 1] <= 'z')
 			{
 				p[i + 1] = p[i + 1] - 32;
-				if (p[i] == seper[1])
-				{
-					p[i] = ' ';
-				}
+
 			}
+			if (p[0] >= 'a' && p[0] <= 'z')
+			{
+				p[0] = p[0] - 32;
+			} 
 		}
 	}
 	return (p);
