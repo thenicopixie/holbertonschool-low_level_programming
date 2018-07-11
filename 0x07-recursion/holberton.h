4f4d5410773125a10b9cb5bde977b5e8816aaea5
thenicopixie <391@holbertonschool.com>
2018-07-11 15:34:03 +0000

    add header file

---
+++ 0x07-recursion/holberton.h
@@ -0,0 +1,26 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * _putchar - function to print one character
+ * _puts_recursion - print a string followed by a new line
+ * _print_rev_recursion - print string in reverse
+ * _strlen_recursion - print length of string
+ * factorial - return factorial of given number
+ * _pow_recursion - return the value of x to the power of y
+ * _sqrt_recursion - return the natural square root of a number
+ * is_prime_number - return 1 is integer is prime
+ * is_palindrome - return 1 if string in palindrome
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+
+#endif /* _HOLBERTON_H_ */
