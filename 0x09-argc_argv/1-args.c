9a8f326cacd3ce1d58f9ddf5c825ad2acc55cfab
thenicopixie <391@holbertonschool.com>
2018-07-13 20:54:31 +0000

    add return description

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: the number of arguments passed
+ * @argv: pointer to each program passed
+ * Return: (0)
+ */
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
