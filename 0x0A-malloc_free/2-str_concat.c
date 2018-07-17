7a330fce33a111e3d2be82f0f87c0705143a300c
thenicopixie <391@holbertonschool.com>
2018-07-17 19:38:39 +0000

    check if NULL is passed

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -13,11 +13,23 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0, len2 = 0, i, j;
 
 	char *ptr;
-
+/* get length of s1 */
 	while (s1[len1] != '\0')
 		len1++;
+/* get length of s2 */
 	while (s2[len2] != '\0')
 		len2++;
+/* allocate memory for ptr */
+	if (len1 == 0)
+	{
+		s1 = malloc(sizeof(char) * 1);
+		s1[0] = ' ';
+	}
+	if (len2 == 0)
+	{
+		s2 = malloc(sizeof(char) * 1);
+		s2[0] = ' ';
+	}
 	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (ptr == NULL)
 	{
