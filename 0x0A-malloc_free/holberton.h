43423e676d403bb6398e3a48845b8abf2f70b129
thenicopixie <391@holbertonschool.com>
2018-07-17 19:03:13 +0000

    update header

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -5,7 +5,7 @@
  * _putchar - print character
  * create_array - creates an array of chars, and initializes it with a specific char
  * _strdup - returns a pointer to a newly allocated space in memory
- *
+ * str_concat - concatenate two strings
  *
  *
  */
@@ -13,5 +13,6 @@
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 #endif /* _HOLBERTON_H_ */
