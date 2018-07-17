5051f7fd3d5c90e8228383ec6d784f8f8044f5ea
thenicopixie <391@holbertonschool.com>
2018-07-17 19:56:06 +0000

    check if strings are NULL

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -13,23 +13,32 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0, len2 = 0, i, j;
 
 	char *ptr;
-/* get length of s1 */
+
+	if (s1 == NULL)
+	{
+		s1 = malloc(sizeof(char));
+		if (s1 == NULL)
+		{
+			return (NULL);
+		}
+		*s1 = '\0';
+	}
+	if (s2 == NULL)
+	{
+		s2 = malloc(sizeof(char));
+		if (s2 == NULL)
+		{
+			return (NULL);
+		}
+		*s2 = '\0';
+	}
+	/* get length of s1 */
 	while (s1[len1] != '\0')
 		len1++;
-/* get length of s2 */
+	/* get length of s2 */
 	while (s2[len2] != '\0')
 		len2++;
-/* allocate memory for ptr */
-	if (len1 == 0)
-	{
-		s1 = malloc(sizeof(char) * 1);
-		s1[0] = ' ';
-	}
-	if (len2 == 0)
-	{
-		s2 = malloc(sizeof(char) * 1);
-		s2[0] = ' ';
-	}
+	/* allocate memory for ptr */
 	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (ptr == NULL)
 	{
