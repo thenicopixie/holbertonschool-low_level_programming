1bff10df0d440816041b2c61e9df207457fd4a73
thenicopixie <391@holbertonschool.com>
2018-07-17 18:53:09 +0000

    update header

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -2,9 +2,9 @@
 #define _HOLBERTON_H_
 
 /**
- * _putchar: print character
- * create_array: creates an array of chars, and initializes it with a specific char
- *
+ * _putchar - print character
+ * create_array - creates an array of chars, and initializes it with a specific char
+ * _strdup - returns a pointer to a newly allocated space in memory
  *
  *
  *
@@ -12,5 +12,6 @@
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif /* _HOLBERTON_H_ */
