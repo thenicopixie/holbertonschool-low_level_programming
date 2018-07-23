edc0aa8307966a0fd88f5dfd9f84742a7448297a
thenicopixie <391@holbertonschool.com>
2018-07-23 20:01:53 +0000

    change condition in age statement

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		(d->name) ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
-		(d->age) ? printf("Age: %f\n", d->age) : printf("Age: (nil)\n");
+		(d->age != 0) ? printf("Age: %f\n", d->age) : printf("Age: (nil)\n");
 		(d->owner) ? printf("Owner: %s\n", d->owner) : printf("Owner: (nil)\n");
 	}
 }
