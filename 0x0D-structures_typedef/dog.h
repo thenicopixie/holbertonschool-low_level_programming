63690cd4ce01bca47a132252ea89b76ca2f15c11
thenicopixie <391@holbertonschool.com>
2018-07-23 19:41:34 +0000

    update

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -16,4 +16,7 @@ struct dog
 int _putchar(char c);
 /* init_dog - function to initialize a variable of type struct dog */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+/* print_dog - print elements in struct */
+void print_dog(struct dog *d);
+
 #endif /* _DOG_H */
