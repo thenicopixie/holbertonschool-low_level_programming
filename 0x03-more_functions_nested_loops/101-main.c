cfdb32a1859f680b099e77eeac003d0516e5626f
thenicopixie <391@holbertonschool.com>
2018-07-25 22:42:42 +0000

    add main files

---
+++ 0x03-more_functions_nested_loops/101-main.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_number(945678);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-2234598);
+    _putchar('\n');
+    return (0);
+}
