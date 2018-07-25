ce3822af87257c478e974f95cd2ce9be1d551281
thenicopixie <391@holbertonschool.com>
2018-07-25 22:12:33 +0000

    add comments

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -10,16 +10,20 @@
  */
 int main(int argc, char **argv)
 {
+	/* if there is more than one character in the operator argument */
 	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	/* check is there are more than 4 arguments passed */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	/* pass arguements into function to check operator */
+	/* print result */
 	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
