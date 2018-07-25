65d9945ee9e719ae2b6661e871fa2d92611cc9a9
thenicopixie <391@holbertonschool.com>
2018-07-25 22:15:09 +0000

    add comments

--- 0x0E-function_pointers/3-calc.h
+++ 0x0E-function_pointers/3-calc.h
@@ -12,11 +12,16 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
-
+/* addition function */
 int op_add(int a, int b);
+/* substraction function */
 int op_sub(int a, int b);
+/* multiplication function */
 int op_mul(int a, int b);
+/* division function */
 int op_div(int a, int b);
+/* modulo function */
 int op_mod(int a, int b);
+/* check if value matches operator */
 int (*get_op_func(char *s))(int, int);
 #endif /* _3_CALC_H_ */
