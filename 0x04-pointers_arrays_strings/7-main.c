caa22aefc31dec94133509846ad6bc3c43ff2d35
thenicopixie <391@holbertonschool.com>
2018-07-25 22:42:27 +0000

    add main files

---
+++ 0x04-pointers_arrays_strings/7-main.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "034125";
+    puts_half(str);
+   
+str = "123";
+    puts_half(str);
+
+ return (0);
+}
