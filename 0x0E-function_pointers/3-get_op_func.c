8e21d7744746028fa9ac83864c22ee39c3cea2cf
thenicopixie <391@holbertonschool.com>
2018-07-25 21:26:13 +0000

    check if matching operator

---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function to perform the operation asked
+ * @s: operator passed as argument to the program
+ * Return: pointer to correct function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	unsigned int i;
+
+	i = 0;
+
+	while (i < sizeof(ops) / sizeof(ops[0]))
+	{
+		if (*s == *(ops[i].op))
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	printf("Error/n");
+	exit(99);
+}
