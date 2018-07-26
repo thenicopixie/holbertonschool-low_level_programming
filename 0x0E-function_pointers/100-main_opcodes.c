9c049401e0c608dfbf3969f13d2497931d3d216c
thenicopixie <391@holbertonschool.com>
2018-07-26 20:43:34 +0000

    print opcodes of main function

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: arguments passed
+ * Return: 0 or exit
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < atoi(argv[1]); i++)
+	{
+		printf("%02x", *((unsigned char *)main + i));
+
+		if (i < atoi(argv[1]) - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
