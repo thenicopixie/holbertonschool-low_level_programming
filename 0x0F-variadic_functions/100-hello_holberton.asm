adaa3081e74722b48efcf28629a39cfd74c09770
thenicopixie <391@holbertonschool.com>
2018-07-30 04:50:40 +0000

    remove typo

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,12 +1,12 @@
 section  .data
-	msg db "Hello, Hoberton", 10
+	message: db "Hello, Hoberton", 10
 section .text
 	global main
 main:
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, msg
-	mov rdx, 17wq
+	mov rsi, message
+	mov rdx, 17
 
 	syscall
 	mov rax, 60
