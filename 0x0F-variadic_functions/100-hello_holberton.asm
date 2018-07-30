fbf631134c7261c63b26453655945aa9aa3d0ad4
thenicopixie <391@holbertonschool.com>
2018-07-30 05:05:33 +0000

    fix spacing

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,15 +1,14 @@
 section .text
     global main
 main:
-    mov    rax, 1
-    mov    rdi, 1
-    mov	   rsi, message
-    mov	   rdx, 17
-
+    mov     rax, 1
+    mov     rdi, 1
+    mov	    rsi, message
+    mov	    rdx, 17
     syscall
-    mov    rax, 60
-    xor    rdi, 0
+    mov     rax, 60
+    xor     rdi, 0
     syscall
 
 section  .data
-    message: db "Hello, Hoberton", 10
+    messag db		"Hello, Hoberton", 10
