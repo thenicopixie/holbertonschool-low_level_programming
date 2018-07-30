62eb5331acdc1008cc1d78be60a9e0dc05c65bc4
thenicopixie <391@holbertonschool.com>
2018-07-30 04:48:56 +0000

    update

---
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,14 @@
+section  .data
+	msg db "Hello, Hoberton", 10
+section .text
+	global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 17wq
+
+	syscall
+	mov rax, 60
+	mov rdi, 0
+	syscall
