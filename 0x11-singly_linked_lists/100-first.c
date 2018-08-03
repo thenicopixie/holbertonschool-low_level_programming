55addde69ade0afed9ae4811f9c73f54b689c4ce
thenicopixie <391@holbertonschool.com>
2018-08-03 01:12:50 +0000

    printf string before main function executes

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * str_print - print string
+ */
+__attribute__ ((constructor))
+void str_print (void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
