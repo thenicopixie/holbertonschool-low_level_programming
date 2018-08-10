b3090889a2920d31dde687219ecb2b00cb02a898
thenicopixie <391@holbertonschool.com>
2018-08-10 00:10:18 +0000

    change while loop argument

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int bits = n ^ m;
 	int count = 0;
 
-	while (bits > 0)
+	while (bits)
 	{
 		bits  = bits & (bits - 1);
 		count++;
