cf71f9250b1feb5ad5404508a44fb59c1a9914ac
thenicopixie <391@holbertonschool.com>
2018-08-14 18:49:18 +0000

    function to read text file and print to standard output

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - function that reads a text file and
+ * prints it to the POSIX standard output
+ * @filename: name of file
+ * @letters: the number of letters read and printed
+ * Return: number of letters read and printed or 0 if failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	int fdes = 0, reader, writer;
+
+	/* read */
+
+	if (!filename)
+		return (0);
+
+	fdes = open(filename, O_RDONLY);
+
+	if (fdes == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (!buffer)
+		return (0);
+
+	reader = read(fdes, buffer, letters);
+
+	if (reader == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	close(fdes);
+
+	/* write */
+
+	fdes = open(filename, O_WRONLY);
+
+	if (fdes == -1)
+		return (0);
+
+	writer = write(fdes, buffer, reader);
+
+	if (writer == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	return (writer);
+}
