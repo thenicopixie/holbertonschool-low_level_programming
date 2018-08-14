a8c261aa41d56bdc7ce50bfdb02153e7fb45b3de
thenicopixie <391@holbertonschool.com>
2018-08-14 20:30:23 +0000

    remove extra variable, change failure return to -1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fdes, writer, length;
+	int fdes, length;
 
 	if (!filename)
 		return (-1);
@@ -20,19 +20,14 @@ int create_file(const char *filename, char *text_content)
 	fdes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fdes == -1)
-		return (0);
+		return (-1);
 
 	/* get length of string in text_content */
 	for (length = 0; text_content[length] != '\0'; length++)
 	;
 
 	/* write contents of test_content into new file */
-	writer = write(fdes, text_content, length);
-
-	if (writer == -1)
-	{
-		return (-1);
-	}
+	write(fdes, text_content, length);
 
 	close(fdes);
 
