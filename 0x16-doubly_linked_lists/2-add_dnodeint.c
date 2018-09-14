66eb73abc104f36ae936e77a665579c3cfd691cf
thenicopixie <391@holbertonschool.com>
2018-09-14 09:09:44 +0000

    function that adds new node at beginning of doulby linked list and returns its address

---
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - function that adds a new node at the beginning of a doubly
+ * linked list
+ * @head: double pointer to doubly linked list
+ * @n: integer value to assign to new node
+ * Return: the address of the new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(*head));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	if (*head)
+		(*head)->prev = new_node;
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+}
