96d5926c2635ca37af72ab04fbaf867a9323e457
thenicopixie <391@holbertonschool.com>
2018-09-14 10:08:06 +0000

    type cast malloc

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,28 +11,21 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node, *temp;
 
-	new_node = malloc(sizeof(*head));
+	new_node = (dlistint_t *)malloc(sizeof(*head) * 8);
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
+	new_node->next = NULL;
 	if (!(*head))
 	{
 		new_node->next = *head;
-		new_node->prev = NULL;
 		*head = new_node;
 		return (new_node);
 	}
 	temp = *head;
-	while (temp)
-	{
-		if (!temp->next)
-		{
-			new_node->next = NULL;
-			new_node->prev = temp;
-			temp->next = new_node;
-			return (new_node);
-		}
+	while (temp->next)
 		temp = temp->next;
-	}
+	temp->next = new_node;
+	new_node->prev = temp;
 	return (new_node);
 }
