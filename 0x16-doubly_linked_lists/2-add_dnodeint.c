652928b5565c42d87e3649420a4bcfbb0d6afec4
thenicopixie <391@holbertonschool.com>
2018-09-14 10:07:41 +0000

    type case malloc statement

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
-	new_node = malloc(sizeof(*head));
+	new_node = (dlistint_t *)malloc(sizeof(*head) * 8);
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
