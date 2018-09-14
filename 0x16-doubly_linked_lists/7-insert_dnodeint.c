c03689cda85da27f7457ff814338210a621a7234
thenicopixie <391@holbertonschool.com>
2018-09-14 11:06:53 +0000

    function that inserts a new node at a given index and returns the address of the new node

---
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index -function that inserts a new node at index n
+ * @h: double pointer to doubly linked list
+ * @idx: index of new node
+ * @n: integer data in new node
+ * Return: address of new node or NULL if failure
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *temp;
+
+	new_node = malloc(sizeof(*h));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+
+	if (!idx)
+	{
+		new_node->next = *h;
+		new_node->prev = NULL;
+		*h = new_node;
+		return (new_node);
+	}
+	if (!(*h))
+		return (NULL);
+
+	temp = *h;
+
+	while (idx > 0)
+	{
+		if (!temp)
+			return (NULL);
+		temp = temp->next;
+		idx--;
+	}
+	new_node->next = temp->next;
+	temp->next->prev = new_node;
+	temp->next = new_node;
+	return (new_node);
+}
