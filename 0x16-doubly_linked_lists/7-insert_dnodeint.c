8f3463c6d373f489bf781402267616f748d06eec
thenicopixie <391@holbertonschool.com>
2018-09-15 20:54:30 +0000

    make previous pointer to new node

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,8 +35,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i++;
 	}
 	new_node->next = temp->next;
-	if (temp->next)
-		temp->next->prev = new_node;
+	new_node->prev = temp;
+	if (new_node->next)
+		new_node->next->prev = new_node;
 	temp->next = new_node;
 	return (new_node);
 }
