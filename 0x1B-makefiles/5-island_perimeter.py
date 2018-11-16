c8264197d2e51ff5cb8306f3d9b8cbd39adacc78
thenicopixie <391@holbertonschool.com>
2018-11-16 23:36:55 +0000

    change loop to use range. start at index 1

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -6,21 +6,17 @@ def island_perimeter(grid):
     """
     Returns the perimeter of the island described in grid
     """
-    perimeter = 1
-    above = -1
-    below = 1
-    for row in grid:
-        for col in row:
-            if row[col] == 1:
-                if row[col - 1] == 0:
+    perimeter = 0
+    for row in range(1, len(grid)-1):
+        for col in range(1, len(grid[0])-1):
+            if grid[row][col] == 1:
+                if grid[row][col - 1] == 0:
                     perimeter += 1
-                if row[col + 1] == 0:
+                if grid[row][col + 1] == 0:
                     perimeter += 1
-                if grid[above][col] == 0:
+                if grid[row - 1][col] == 0:
                     perimeter += 1
-                if grid[below][col] == 0:
+                if grid[row - 1][col] == 0:
                     perimeter += 1
-        above += 1
-        below += 1
     return perimeter
 
