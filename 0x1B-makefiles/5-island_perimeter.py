5f75b046df8c9fbbcaeda0feaef88155de32cc1f
thenicopixie <391@holbertonschool.com>
2018-11-17 02:54:51 +0000

    update to account for perimeter of grid

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -12,19 +12,13 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if grid[row][col] == 1:
-                if grid[row][col - 1] == 0:
+                if grid[row][col - 1] == 0 or col - 1 < 0:
                     perimeter += 1
-                if grid[row][col + 1] == 0:
+                if grid[row + 1][col] == 0 or row + 1 >= len(grid):
                     perimeter += 1
-                if grid[row - 1][col] == 0:
+                if grid[row - 1][col] == 0 or row - 1 < 0:
                     perimeter += 1
-                if grid[row - 1][col] == 0:
-                    perimeter += 1
-                if row - 1 < 0:
-                    perimeter += 1
-                if row + 1 >= len(grid):
-                    perimeter += 1
-                if col == 0 or col == len(grid[0]):
+                if grid[row][col + 1] == 0 or col + 1 >= len(grid[0]):
                     perimeter += 1
 
     return perimeter
