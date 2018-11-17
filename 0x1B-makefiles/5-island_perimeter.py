4230d748a9bfad4bd78aa02799db869f6faf2c72
thenicopixie <391@holbertonschool.com>
2018-11-17 00:06:44 +0000

    add cases for out of bounds

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -9,8 +9,8 @@ def island_perimeter(grid):
     Returns the perimeter of the island described in grid
     """
     perimeter = 0
-    for row in range(len(grid)-1):
-        for col in range(len(grid[0])-1):
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
             if grid[row][col] == 1:
                 if grid[row][col - 1] == 0:
                     perimeter += 1
@@ -20,4 +20,11 @@ def island_perimeter(grid):
                     perimeter += 1
                 if grid[row - 1][col] == 0:
                     perimeter += 1
+                if row - 1 < 0:
+                    perimeter += 1
+                if row + 1 >= len(grid):
+                    perimeter += 1
+                if col == 0 or col == len(grid[0]):
+                    perimeter += 1
+
     return perimeter
