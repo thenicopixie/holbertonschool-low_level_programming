37ca45381e333975e09ecae626981117ff24f2b2
thenicopixie <391@holbertonschool.com>
2018-12-12 21:26:06 +0000

    split up if statement arguments

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -7,7 +7,9 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->left == NULL && node->right == NULL || node != NULL)
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
 		return (1);
 	else
 		return (0);
